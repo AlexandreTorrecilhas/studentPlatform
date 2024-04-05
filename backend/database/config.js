03f5d188943c02c7471c1540cfaaa546aa1172e1
AlexandreTorrecilhas <carlos.torrecilhas@outlook.com>
2024-04-05 10:56:08 -0300

    The database's config file was created. Now, we are creating the file that connects with the database

--- backend/database/config.js
+++ backend/database/config.js
@@ -1,7 +1,20 @@
 const dotenv = require('dotenv');
 const path = require('path');
-const dotenvPath = path.resolve(__dirname, '')
+const dotenvPath = path.resolve(__dirname, '../../env/localhost.env');
+dotenv.config({path: dotenvPath});
 
 module.exports = {
-    
+    development: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT
+    },
+    test:{
+
+    },
+    production: {
+
+    }
 }
